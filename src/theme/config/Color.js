087a9ef6880e3f1700be4838e41065a3de440531
valentinEutrope <valentin.eutrope@free.fr>
2019-08-27 13:53:03 +0200

    feat(client: theme): implement theme base

---
+++ src/theme/config/Color.js
@@ -0,0 +1,14 @@
+import palette from "../defaultColors";
+
+export const primary = palette.blue;
+export const secondary = palette.yellow;
+export const tertiary = palette.darkenGrey;
+
+export const info = palette.blueSky;
+
+export default {
+  primary,
+  secondary,
+  tertiary,
+  info
+};
---
+++ src/theme/index.js
@@ -0,0 +1,7 @@
+import Font from "./config/Font";
+import Color from "./config/Color";
+
+export default {
+  ...Font,
+  ...Color
+};
